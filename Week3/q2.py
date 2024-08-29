6d21488af0022a785ac02589d3a057a0b905cb21
AkashGola42 <golaakash856@gmail.com>
2024-08-29 15:29:09 +0530

    Add files via upload

---
+++ Week3/q2.py
@@ -0,0 +1,14 @@
+
+num = 1
+for i in range(10):
+    for j in range(num):
+        print("*", end =" ")
+    num += 1
+    print("")
+
+num1 = 4
+for i in range(9):
+    for j in range(num1):
+        print("*", end =" ")
+    num1 -= 1
+    print("")
---
+++ Week3/q3.py
@@ -0,0 +1,6 @@
+text = input("Enter the string: ")
+
+length = len(text)
+num = 0
+for i in range(0,length,2):
+        print(text[i], end="")
